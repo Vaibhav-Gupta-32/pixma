1864d77d8d479ef6bbd0149da28c98ddc586b60c
Deekshay22 <deekshayadav4422@gmail.com>
2025-05-10 16:41:13 +0530

    10-5-25

--- index.php
+++ index.php
@@ -451,203 +451,74 @@ $tblkey = "id";
 </div>  -->
 
 <!-- Service Industries start -->
-<div class="service section-padding position-relative">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay=".1s">
-
-                <div class="heading-one text-center">
-                    <!-- <span class="heading-one-subtitle">Our Services</span> -->
-                    <h2 class="heading-one-title">Industries We Serve</h2>
-                    <!-- <p>Kimod tempoer incididunt onomes sundo ritoma amar Lorem ipsum dolor sit amet, consectetur adipisicing</p> -->
-                </div>
-                <!-- Heading End -->
+<!--  -->
+<section class="vs-container pt-5">
+    <div class="row m-2">
+        <div class="col-md-12 text-md-center pt-4 vs-hline">
+            <h3 vs-edit="" class="m-3"> <b>Industries we server</b> <span class="d-inline d-sm-none"><br></span></h3>
+        </div> <a class="col-md-4 media-content-block" href="https://www.viewsonic.com/in/education/">
+            <div class="hidden-more">
+                <div class="media-content-bg lazyloaded" style="background: url(https://www.viewsonic.com/vsAssetFile/in/img/resize/home/Encourage-13.webp) 50% 50% / cover no-repeat;" data-src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/Encourage-13.webp"><img src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/spacebg.webp" class="img-fluid lazyloaded" alt="spacebg"></div>
             </div>
-        </div>
-        <div class="row row-cols-lg-3 row-cols-md-2 row-cols-sm-2 row-cols-1 mb-n30">
-            <div class="col mb-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay=".1s">
-                <!-- Single Service Start -->
-                <div class="service-single">
-                    <div class="service-single-image">
-                        <div class="service-single-image-thumb">
-                            <img class="rounded-2" src="assets/images/service/industries/7.jpg" alt="ServiceImage">
-                        </div>
-                        <div class="service-single-image-content">
-                            <div class="icon">
-                                <img src="assets/images/icon/service/education.png" alt="Icon">
-                            </div>
-                            <h4 class="title">Educational <br> Institutions</h4>
-                        </div>
-                    </div>
-                    <div class="service-single-white">
-                        <a href="javascript:void()" class="service-single-white-content">
-                            <div class="icon gradient-1">
-                                <img src="assets/images/icon/service/icon-1-white.png" alt="Icon">
-                            </div>
-                            <span class="title">Educational <br> Institutions</span>
-                            <p>Educational Institutions Enhance interactive learning with digital flat panels and real-time collaboration tools.</p>
-                        </a>
-                    </div>
-                </div>
-                <!-- Single Service End -->
+            <div class="py-3">
+                <h4 vs-edit="">Education Institute</h4>
+                <p vs-edit="">Educational Institutions Enhance interactive learning with digital flat panels and real-time collaboration tools.</p>
             </div>
-            <div class="col mb-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay=".3s">
-                <!-- Single Service Start -->
-                <div class="service-single">
-                    <div class="service-single-image">
-                        <div class="service-single-image-thumb">
-                            <img src="assets/images/service/industries/4.jpg" alt="ServiceImage">
-                        </div>
-                        <div class="service-single-image-content">
-                            <div class="icon">
-                                <img src="assets/images/icon/service/corporate-office.png" alt="Icon">
-                            </div>
-                            <h4 class="title">Corporate <br> Offices </h4>
-                        </div>
-                    </div>
-                    <div class="service-single-white">
-                        <a href="javascript:void()" class="service-single-white-content">
-                            <div class="icon gradient-1">
-                                <img src="assets/images/icon/service/icon-1-white.png" alt="Icon">
-                            </div>
-                            <span class="title">Corporate <br> Offices</span>
-                            <p> Streamline meetings with video conferencing, digital podiums, and professional display solutions. </p>
-                        </a>
-                    </div>
-                </div>
-                <!-- Single Service End -->
+            <div class="vs-read-more"> <span vs-edit="">Learn More &nbsp; <i class="fa fa-angle-right" aria-hidden="true"></i></span> </div>
+        </a> <a class="col-md-4 media-content-block" href="https://www.viewsonic.com/in/work/">
+            <div class="hidden-more">
+                <div class="media-content-bg lazyloaded" style="background: url(https://www.viewsonic.com/vsAssetFile/in/img/resize/home/banner_viewboard.webp) 50% 50% / cover no-repeat;" data-src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/banner_viewboard.webp"><img src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/spacebg.webp" class="img-fluid lazyloaded" alt="spacebg"></div>
             </div>
-            <div class="col mb-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay=".6s">
-                <!-- Single Service Start -->
-                <div class="service-single">
-                    <div class="service-single-image">
-                        <div class="service-single-image-thumb">
-                            <img src="assets/images/service/industries/8.jpg" alt="ServiceImage">
-                        </div>
-                        <div class="service-single-image-content mt-4">
-                            <div class="icon">
-                                <img src="assets/images/icon/service/retail & showroom.png" alt="Icon">
-                            </div>
-                            <h4 class="title"> Retail <br> & <br> Showrooms</h4>
-                        </div>
-                    </div>
-                    <div class="service-single-white">
-                        <a href="javascript:void()" class="service-single-white-content">
-                            <div class="icon gradient-1">
-                                <img src="assets/images/icon/service/icon-1-white.png" alt="Icon">
-                            </div>
-                            <span class="title"> Retail <br> & <br> Showrooms</span>
-                            <p> Attract customers with dynamic digital signage and engaging promotional displays. </p>
-                        </a>
-                    </div>
-                </div>
-                <!-- Single Service End -->
+            <div class="py-3">
+                <h4 vs-edit="">Corporate Office</h4>
+                <p vs-edit=""> Streamline meetings with video conferencing, digital podiums, and professional display solutions. </p>
             </div>
-            <div class="col mb-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay=".1s">
-                <!-- Single Service Start -->
-                <div class="service-single">
-                    <div class="service-single-image">
-                        <div class="service-single-image-thumb">
-                            <img src="assets/images/service/industries/9.jpg" alt="ServiceImage">
-                        </div>
-                        <div class="service-single-image-content">
-                            <div class="icon">
-                                <img src="assets/images/icon/service/event-venue.png" alt="Icon">
-                            </div>
-                            <h4 class="title">Event <br> Venues</h4>
-                        </div>
-                    </div>
-                    <div class="service-single-white">
-                        <a href="javascript:void()" class="service-single-white-content">
-                            <div class="icon gradient-1">
-                                <img src="assets/images/icon/service/icon-1-white.png" alt="Icon">
-                            </div>
-                            <span class="title">Event <br>Venues</span>
-                            <p> Elevate conferences and exhibitions with interactive standees and live presentation tools.
-                            </p>
-                        </a>
-                    </div>
-                </div>
-                <!-- Single Service End -->
+            <div class="vs-read-more"> <span vs-edit="">Find a Solution &nbsp; <i class="fa fa-angle-right" aria-hidden="true"></i></span> </div>
+        </a> <a class="col-md-4 media-content-block" href="https://www.viewsonic.com/in/home/">
+            <div class="hidden-more">
+                <div class="media-content-bg lazyloaded" style="background: url(https://www.viewsonic.com/vsAssetFile/in/img/resize/home/frontpage-home.webp) 50% 50% / cover no-repeat;" data-src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/frontpage-home.webp" uk-img=""><img src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/spacebg.webp" class="img-fluid lazyloaded" alt="spacebg"></div>
             </div>
-            <div class="col mb-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay=".3s">
-                <!-- Single Service Start -->
-                <div class="service-single">
-                    <div class="service-single-image">
-                        <div class="service-single-image-thumb">
-                            <img src="assets/images/service/industries/3.jpg" alt="ServiceImage">
-                        </div>
-                        <div class="service-single-image-content mt-4">
-                            <div class="icon">
-                                <img src="assets/images/icon/service/health-care.png" alt="Icon" style="margin-top: 13px;">
-                            </div>
-                            <h4 class="title">Hospitals <br> Healthcare <br> Centers</h4>
-                        </div>
-                    </div>
-                    <div class="service-single-white">
-                        <a href="javascript:void()" class="service-single-white-content">
-                            <div class="icon gradient-1">
-                                <img src="assets/images/icon/service/icon-1-white.png" alt="Icon">
-                            </div>
-                            <span class="title">Hospitals <br> Healthcare <br> Centers</span>
-                            <p> Improve patient communication with self-check-in kiosks and digital information boards.
-                            </p>
-                        </a>
-                    </div>
-                </div>
-                <!-- Single Service End -->
+            <div class="py-3">
+                <h4 vs-edit="">Retail & Showrooms</h4>
+                <p vs-edit="">Attract customers with dynamic digital signage and engaging promotional displays. </p>
             </div>
-            <div class="col mb-30 wow fadeInUp" data-wow-duration="1.5s" data-wow-delay=".6s">
-                <!-- Single Service Start -->
-                <div class="service-single">
-                    <div class="service-single-image">
-                        <div class="service-single-image-thumb">
-                            <img src="assets/images/service/industries/2.jpg" alt="ServiceImage">
-                        </div>
-                        <div class="service-single-image-content">
-                            <div class="icon">
-                                <img src="assets/images/icon/service/hotel.png" alt="Icon">
-                            </div>
-                            <h4 class="title">Hotels <br> Hospitality </h4>
-                        </div>
-                    </div>
-                    <div class="service-single-white">
-                        <a href="javascript:void()" class="service-single-white-content">
-                            <div class="icon gradient-1">
-                                <img src="assets/images/icon/service/icon-1-white.png" alt="Icon">
-                            </div>
-                            <span class="title">Hotels <br> Hospitality </span>
-                            <p>Enhance guest experience with interactive kiosks, digital signages, and smart display solutions.
-                            </p>
-                        </a>
-                    </div>
-                </div>
-                <!-- Single Service End -->
+            <div class="vs-read-more"> <span vs-edit="">Explore &nbsp; <i class="fa fa-angle-right" aria-hidden="true"></i></span> </div>
+        </a>
+    </div>
+    
+    <div class="row m-2">
+        <div class="col-md-12 text-md-center pt-4 vs-hline">
+            <!-- <h3 vs-edit="">Solutions for <span class="d-inline d-sm-none"><br></span>Learning, Work, and Play</h3> -->
+        </div> <a class="col-md-4 media-content-block" href="https://www.viewsonic.com/in/education/">
+            <div class="hidden-more">
+                <div class="media-content-bg lazyloaded" style="background: url(https://www.viewsonic.com/vsAssetFile/in/img/resize/home/Encourage-13.webp) 50% 50% / cover no-repeat;" data-src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/Encourage-13.webp"><img src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/spacebg.webp" class="img-fluid lazyloaded" alt="spacebg"></div>
             </div>
-        </div>
+            <div class="py-3">
+                <h4 vs-edit="">Event & Venues</h4>
+                <p vs-edit="">Elevate conferences and exhibitions with interactive standees and live presentation tools.</p>
+            </div>
+            <div class="vs-read-more"> <span vs-edit="">Learn More &nbsp; <i class="fa fa-angle-right" aria-hidden="true"></i></span> </div>
+        </a> <a class="col-md-4 media-content-block" href="https://www.viewsonic.com/in/work/">
+            <div class="hidden-more">
+                <div class="media-content-bg lazyloaded" style="background: url(https://www.viewsonic.com/vsAssetFile/in/img/resize/home/banner_viewboard.webp) 50% 50% / cover no-repeat;" data-src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/banner_viewboard.webp"><img src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/spacebg.webp" class="img-fluid lazyloaded" alt="spacebg"></div>
+            </div>
+            <div class="py-3">
+                <h4 vs-edit="">Hospitals & Healthcare <br> Centers</h4>
+                <p vs-edit="">Improve patient communication with self-check-in kiosks and digital information boards.</p>
+            </div>
+            <div class="vs-read-more"> <span vs-edit="">Find a Solution &nbsp; <i class="fa fa-angle-right" aria-hidden="true"></i></span> </div>
+        </a> <a class="col-md-4 media-content-block" href="https://www.viewsonic.com/in/home/">
+            <div class="hidden-more">
+                <div class="media-content-bg lazyloaded" style="background: url(https://www.viewsonic.com/vsAssetFile/in/img/resize/home/frontpage-home.webp) 50% 50% / cover no-repeat;" data-src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/frontpage-home.webp" uk-img=""><img src="https://www.viewsonic.com/vsAssetFile/in/img/resize/home/spacebg.webp" class="img-fluid lazyloaded" alt="spacebg"></div>
+            </div>
+            <div class="py-3">
+                <h4 vs-edit="">Hotels Hospitality </h4>
+                <p vs-edit="">Enhance guest experience with interactive kiosks, digital signages, and smart display solutions.</p>
+            </div>
+            <div class="vs-read-more"> <span vs-edit="">Explore &nbsp; <i class="fa fa-angle-right" aria-hidden="true"></i></span> </div>
+        </a>
     </div>
-
-    <h2 class="rotate-title rotate-right d-none d-xxl-block">INDUSTRIES</h2>
-
-    <div class="shape shape-1">
-        <img src="assets/images/shape/dot-2.png" alt="Shape">
-    </div>
-    <div class="shape shape-2">
-        <img src="assets/images/shape/shape-7.png" alt="Shape">
-    </div>
-    <div class="shape shape-3">
-        <img src="assets/images/shape/shape-2.png" alt="Shape">
-    </div>
-    <div class="shape shape-4">
-        <img src="assets/images/shape/shape-9.png" alt="Shape">
-    </div>
-    <div class="shape shape-5">
-        <img src="assets/images/shape/dot-1.png" alt="Shape">
-    </div>
-    <div class="shape shape-6">
-        <img src="assets/images/shape/shape-10.png" alt="Shape">
-    </div>
-</div>
+</section>
 <!-- Service Section End -->
 
 <!-- Work Two Section Start -->
